c270c21098357ac813e8baa07794764fb4cb233b
VickyyGerb <vickyag2007@gmail.com>
2025-07-22 21:01:30 -0300

    arreglos

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './App.css'
 function App() {
 
   return (
-    <div className="mt-4"> //mt seria margin-top, mb margin bottom, ml margin left, mr margin right, m margin all y con el paddin es lo mismo pero con p
+    <div className="mt-5"> {/*mt seria margin-top, mb margin bottom, ml margin left, mr margin right, m margin all y 
+    con el paddin es lo mismo pero con p*/}
       <h1 className='mb-5 text-5xl font-bold text-blue-500'>To-Do App</h1>
       <TodoForm/>
     </div>
